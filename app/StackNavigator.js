161b2ab55b4278de338a3e9fc061059e49d376ca
Dylan Stevens <79028721+dylanrstevens@users.noreply.github.com>
2022-10-28 14:05:38 -0700

    Added style to drawer items

--- app/StackNavigator.js
+++ app/StackNavigator.js
@@ -2,22 +2,81 @@ import { View, Text } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from './screens/HomeScreen';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 import BudgetScreen from './screens/BudgetScreen';
+import {
+    CreditCardIcon,
+    ClipboardDocumentCheckIcon
+} from "react-native-heroicons/outline"
+import { flex } from 'react-native-wind/dist/styles/flex/flex';
+import { flexDirections } from 'react-native-wind/dist/styles/flex/flex-direction';
+import { borderWidths } from 'react-native-wind/dist/styles/view/border-width';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
+const CustomDrawer = (props) => {
+
+    return (
+        <DrawerContentScrollView>
+            <View className="">
+                <DrawerItemList {...props}/>
+            </View>
+        </DrawerContentScrollView>
+    )
+}
+
 const StackNavigator = () => {
 
     return (
-        <Drawer.Navigator useLegacyImplementation initialRouteName='Home' screenOptions={
+        <Drawer.Navigator useLegacyImplementation initialRouteName='Your Accounts' screenOptions={
             {
                 headerShown:false
             }
-        }>
-            <Drawer.Screen name="Home" component={HomeScreen}/>
-            <Drawer.Screen name="Budget" component={BudgetScreen}/>
+        }
+        drawerContent={(props) => <CustomDrawer {...props}/>}
+        >
+            <Drawer.Screen 
+                name="Your Accounts"
+                component={HomeScreen}
+                options={{
+                    drawerIcon:({color}) => (
+                        <CreditCardIcon color={color} size={35}/>
+                    ),
+                    drawerItemStyle: {
+                        borderRadius:20,
+                        borderStyle:'solid',
+                        borderWidth:1,
+                        borderColor:'#BBBBBB'
+                    },
+                    drawerLabelStyle: {
+                        fontSize:16,
+                        padding:20
+                    }
+                    
+                }}
+            />
+            <Drawer.Screen
+                name="Budget"
+                component={BudgetScreen}
+                options={{
+                    drawerIcon:({color}) => (
+                        <ClipboardDocumentCheckIcon color={color} size={35}/>
+                    ),
+                    drawerItemStyle: {
+                        borderRadius:20,
+                        borderStyle:'solid',
+                        borderWidth:1,
+                        borderColor:'#BBBBBB'
+                    },
+                    drawerLabelStyle: {
+                        fontSize:16,
+                        padding:20
+                    },
+
+                    
+                }}
+            />
         </Drawer.Navigator>
         
     )
